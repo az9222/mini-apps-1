80d82346eaafe2fcb02a593d77cfcfbbb20c51cb
Angela Zhou <angelazhou922@gmail.com>
2018-10-16 14:42:57 -0700

    Server works

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,2 @@
-
+//build client app here. The client app should be able to submit JSON data to server, receive a response containing a CSV formatted result
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,27 @@
+var express = require('express');
+var app = express();
+const port = 3000;
+// var path = require('path');
 
+//point express to client folder and it will automatically start serving up those files (these files in this folder are static assets). Now by default, express will look for a file called index.html and serve that file whenever you browse to the root '/' route
+app.use(express.static('./client'));
 
+//Example Express Route: `app.post('/upload_json', (req, res) => { ... })`
+// HTML Form: `<form method="POST" action="/upload_json"></form>`
+// app.get('/', (req, res) => {
+//     res.sendFile(express);
+// })
+
+//submitting data via ajax:
+//initiate an ajax request to a url on your server
+// Express Route: `app.post('/upload_json', (req, res) => { ... })`
+// AJAX Method: `$.ajax({method: 'POST', url: '/upload_json', ... })`
+
+//prevent page reload
+// $('form').on('submit', function(e){
+//     e.preventDefault();
+//     // ... more event handler code here
+//   });
+
+//listen to a port
+app.listen(port, () => {console.log('listening to port', port)});
